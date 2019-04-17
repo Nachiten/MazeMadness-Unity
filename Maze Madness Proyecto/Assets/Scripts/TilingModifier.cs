ae975236f6c802e8fce42027b9d063da4e9bb87c
Ignacio Baptista <ibaptista@est.frba.utn.edu.ar>
2019-04-17 18:52:38 -0300

    Tiling automatico
    Version: Alpha 2.4.0
    Cambios:
    - Creado codigo TilingModifier
    - Tiling automatico por codigo
    - Funcionando todos los niveles

---
+++ Maze Madness Proyecto/Assets/Scripts/TilingModifier.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilingModifier : MonoBehaviour
+{
+    int contador = 1;
+
+    Transform transform;
+
+    Renderer textura;
+
+    GameObject piso;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        while (GameObject.Find("Piso " + contador.ToString())) {
+
+            piso = GameObject.Find("Piso " + contador.ToString());
+
+            transform = piso.GetComponent<Transform>();
+            textura = piso.GetComponent<Renderer>();
+
+            // Ajustar "Tiling" de textura
+            textura.material.mainTextureScale = new Vector2(transform.localScale.x / 10, transform.localScale.z / 10);
+
+            Debug.Log("PISO: " + piso);
+            Debug.Log("Transform: " + transform);
+            Debug.Log("Textura: " + textura);
+
+            contador++;
+        }
+    }
+
+}
