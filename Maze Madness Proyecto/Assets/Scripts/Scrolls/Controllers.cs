718b87fddae6d3cd2aec601c4128fd4d89dd5b84
Ignacio Baptista <ibaptista@est.frba.utn.edu.ar>
2019-04-18 01:07:49 -0300

    Optimizados codigos
    Version: Alpha 2.4.0
    Cambios:
    - Optimizados y juntados codigos de Controllers

---
+++ Maze Madness Proyecto/Assets/Scripts/Scrolls/Controllers.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+
+public class Controllers : MonoBehaviour
+{
+    /* --------------------------- Volumen --------------------------- */
+
+    public AudioMixer mixer;
+    GameObject numeroVolumen;
+    Text texto;
+
+    public void SetLevel(float valorSlider)
+    {
+        numeroVolumen = GameObject.FindGameObjectWithTag("Numero Volumen");
+
+        texto = numeroVolumen.GetComponent<Text>();
+
+        texto.text = (valorSlider * 100).ToString("F2");
+
+        valorSlider = valorSlider * 0.9999f + 0.0001f;
+
+        mixer.SetFloat("Volume", Mathf.Log10 (valorSlider) * 20);
+    }
+
+    /* --------------------------- Velocidad --------------------------- */
+
+    public void velocidad(float valor) { GameObject.FindGameObjectWithTag("Jugador").GetComponent<PlayerMovement>().AjustarVelocidad(valor); }
+
+    /* --------------------------- Sensibilidad --------------------------- */
+
+    public void sensibilidad(float valor) { GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraMovement>().AjustarSensibilidad(valor); }
+}
